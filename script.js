6539d471ebdc4647fd61746e62b4a21717a06a26
Mehanthika12 <146060982+Mehanthika12@users.noreply.github.com>
2024-11-11 20:27:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const digitalClock = () => {
+    let datetime = new Date();
+    let hour = padZero(datetime.getHours())
+    let mins = padZero(datetime.getMinutes())
+    let sec = padZero(datetime.getSeconds())
+    let ampm = hour >= 24 ? 'PM' : 'AM';
+
+    document.getElementById("hour").innerHTML = hour
+    document.getElementById("mins").innerHTML = mins
+    document.getElementById("sec").innerHTML = sec
+    document.getElementById("ampm").innerHTML = ampm
+
+}
+
+const padZero = (num) => {
+    return num < 10 ? "0" + num : num
+}
+
+setInterval(digitalClock, 1000)
